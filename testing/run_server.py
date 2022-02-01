70c028bdf476d907d6e2ff003cf83e729e343ed0
Lev Vereshchagin <mail@vrslev.com>
2022-02-01 15:25:07 +0300

    Add testing script

---
+++ testing/run_server.py
@@ -0,0 +1,12 @@
+import time
+
+from jack_server import Server, set_error_function, set_info_function
+
+server = Server(driver="coreaudio", device="BuiltInSpeakerDevice", sync=True)
+server.start()
+
+time.sleep(0.5)
+
+set_error_function(lambda s: None)
+set_info_function(lambda s: None)
+server.stop()
