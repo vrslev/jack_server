a353ace6738d90727ac58b6b67ee52b5eff11452
Lev Vereshchagin <mail@vrslev.com>
2022-02-05 14:20:25 +0300

    Add simple tests

---
+++ tests/test_main.py
@@ -0,0 +1,24 @@
+import sys
+
+import pytest
+
+import jack_server
+
+
+@pytest.fixture
+def driver():
+    if sys.platform == "darwin":
+        return "coreaudio"
+    elif sys.platform == "linux":
+        return "alsa"
+
+
+def test_simple(driver: str):
+    server = jack_server.Server(driver=driver)
+    server.start()
+    assert server._created
+    assert server._opened
+    assert server._started
+    server.stop()
+    assert not server._opened
+    assert not server._started
