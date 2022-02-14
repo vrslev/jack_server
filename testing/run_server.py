0f9ec6a5c385b80a8830861a89706d271283ce44
Lev Vereshchagin <mail@vrslev.com>
2022-02-15 01:14:54 +0300

    Set period in testing script

--- testing/run_server.py
+++ testing/run_server.py
@@ -2,7 +2,9 @@ import time
 
 from jack_server import Server, set_error_function, set_info_function
 
-server = Server(driver="coreaudio", device="BuiltInSpeakerDevice", sync=True)
+server = Server(
+    driver="coreaudio", device="BuiltInSpeakerDevice", sync=True, period=1024
+)
 server.start()
 
 time.sleep(0.5)
