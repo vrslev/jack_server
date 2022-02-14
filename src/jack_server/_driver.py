ceb970d518946a4abdddcf6f1180dc0a032dbf4b
Lev Vereshchagin <mail@vrslev.com>
2022-02-15 00:57:23 +0300

    Add period param to driver, make public params properties

--- src/jack_server/_driver.py
+++ src/jack_server/_driver.py
@@ -1,7 +1,7 @@
 from __future__ import annotations
 
 from ctypes import pointer
-from typing import Literal
+from typing import Literal, cast
 
 import jack_server._lib as lib
 from jack_server._parameter import Parameter, get_params_from_jslist
@@ -15,9 +15,9 @@ class Driver:
 
     def __init__(self, ptr: pointer[lib.jackctl_driver_t]) -> None:
         self.ptr = ptr
-        self._set_params()
+        self._init_params()
 
-    def _set_params(self) -> None:
+    def _init_params(self) -> None:
         params_jslist = lib.jackctl_driver_get_parameters(self.ptr)
         self.params = get_params_from_jslist(params_jslist)
 
@@ -25,11 +25,29 @@ class Driver:
     def name(self) -> str:
         return lib.jackctl_driver_get_name(self.ptr).decode()
 
-    def set_device(self, name: str) -> None:
-        self.params["device"].value = name.encode()
+    @property
+    def device(self) -> str:
+        return cast(bytes, self.params["device"].value).decode()
 
-    def set_rate(self, rate: SampleRate) -> None:
-        self.params["rate"].value = rate
+    @device.setter
+    def device(self, __value: str) -> None:
+        self.params["device"].value = __value.encode()
+
+    @property
+    def rate(self) -> SampleRate:
+        return cast(SampleRate, self.params["rate"].value)
+
+    @rate.setter
+    def rate(self, __value: SampleRate) -> None:
+        self.params["rate"].value = __value
+
+    @property
+    def period(self) -> int:
+        return cast(int, self.params["period"].value)
+
+    @period.setter
+    def period(self, __value: int) -> None:
+        self.params["period"].value = __value
 
     def __repr__(self) -> str:
         return f"<jack_server.Driver name={self.name}>"
