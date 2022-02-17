a649045490c08c6bc0a0b931f7e71fe9e2cc10a6
Lev Vereshchagin <mail@vrslev.com>
2022-02-17 13:39:42 +0300

    Fix CI tests

--- tests/test_driver.py
+++ tests/test_driver.py
@@ -5,12 +5,7 @@ from tests.conftest import check_property
 
 
 @pytest.mark.parametrize(
-    ("name", "value", "param_value"),
-    (
-        ("device", "somedevice", b"somedevice"),
-        ("rate", 41000, 41000),
-        ("period", 256, 256),
-    ),
+    ("name", "value", "param_value"), (("rate", 41000, 41000), ("period", 256, 256))
 )
 def test_driver_properties(driver: str, name: str, value: str, param_value: str):
     server = Server(driver=driver)
