54795de8afb732c720d0defaba9c512c6e2e6a71
e.blackford <e.blackford@klippel.de>
2023-11-01 15:17:30 +0100

    actually check that the library is found in test_lib
    Surely we want this test to fail if JACK is not installed correctly?

--- tests/test_lib.py
+++ tests/test_lib.py
@@ -6,12 +6,10 @@ from jack_server._lib import _lib_names, get_library_name
 
 @pytest.mark.parametrize("name", _lib_names)
 def test_get_library_name_found(monkeypatch: pytest.MonkeyPatch, name: str):
-    def func(v: str):
-        if v == name:
-            return "ok"
 
-    monkeypatch.setattr(jack_server._lib, "find_library", func)
-    assert get_library_name() == "ok"
+    library_name = get_library_name()
+    # check library found and is in list of names
+    assert True in [name in library_name for name in _lib_names]
 
 
 def test_get_library_name_not_found(monkeypatch: pytest.MonkeyPatch):
